1a4cf4d2a38da0ce7ce086cd32c328b341980375
uni <rhcjrdbsgml@gmail.com>
2024-02-20 23:15:42 +0900

    useQuery

--- src/ components/Posts.tsx
+++ src/ components/Posts.tsx
@@ -1,15 +1,15 @@
 "use client"
 import React from 'react'
-import { useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
+import { getData } from '@/app/page';
 
 export default function Posts() {
-    const queryClient = useQueryClient();
-    const data = queryClient.getQueryData(["todos"]);
+    const {data} = useQuery({queryKey: ["todos"], queryFn:getData})
 console.log(data)
   return (
     <div>
-      <p>{data?.id}</p>
-      <p>{data?.title}</p>
+      <p>{data.id}</p>
+      <p>{data.title}</p>
     </div>
   )
 }
